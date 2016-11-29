51afcf917d74990fe6caca09892796545947e488
mayermatheus <matheusmayerduarte@gmail.com>
2016-11-28 21:23:53 -0300

    Implementado as funcionalidades de listagem e renomeacao de usuarios

--- TCP/ServerListener.java
+++ TCP/ServerListener.java
@@ -2,13 +2,21 @@ import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.Socket;
+import java.util.ArrayList;
+import java.util.List;
+
+import model.MaquinaCliente;
 
 
 public class ServerListener extends Thread{
-
+	private List<MaquinaCliente> maquinasClientes = new ArrayList<MaquinaCliente>();
+	private MaquinaCliente maquinaCliente;
 	Socket s;
-	public ServerListener(Socket s) {
-		this.s = s;
+
+	public ServerListener(MaquinaCliente maquinaCliente, List<MaquinaCliente> maquinasClientes) {
+		this.maquinaCliente = maquinaCliente;
+		this.s = maquinaCliente.getSocketCliente();
+		this.maquinasClientes = maquinasClientes;
 	}
 
 	public void run(){
@@ -16,33 +24,36 @@ public class ServerListener extends Thread{
 			String msg = "";
 			do{
 				DataInputStream dataIn = new DataInputStream(s.getInputStream());
-				String op = dataIn.readUTF();
-				System.out.println("'"+op+"'");
-				
+				/**
+				 * @todo isso aqui tá uma verdadeira gambiarraaa, vou pensar em um melhor jeito pra isso depois
+				 */
+				String comando = dataIn.readUTF();
+				String arrayComando[] = comando.split(" ");
+				String op = arrayComando[0];
 				switch(op){
 				case "bye":
 					msg = "fim";
 					s.close();
 					break;
 
-				case "2":
-					msg = "OLAR2";
+				case "list":
+					msg = listarUsuarios();
 					break;
 
-				case "3":
-					msg = "OLAR3";
+				case "rename":
+					msg = this.renameUsuario(arrayComando[1]);
 					break;
 				default:
 					msg = "GZUIZ";
 					break;
 				}		
-				
+
 				DataOutputStream dataOutput = new DataOutputStream(s.getOutputStream());
 				//executa alguma coisa... no caso, um eco.
 				dataOutput.writeUTF(msg);
-				
-//				ServerWriter serverwriter = new ServerWriter(s,msg);
-//				serverwriter.start();
+
+				//				ServerWriter serverwriter = new ServerWriter(s,msg);
+				//				serverwriter.start();
 			}while(!s.isClosed());			
 			s.close();
 
@@ -52,4 +63,48 @@ public class ServerListener extends Thread{
 		}
 	}
 
+	/**
+	 * @author mayer
+	 * @todo Planejar um jeito de jogar esses metodos para o servidor e não para a thread
+	 * @return String
+	 */
+	public String listarUsuarios(){
+		String mensagem = "Usuarios Online:";
+
+		for(MaquinaCliente maquinaCliente: this.maquinasClientes) {
+			mensagem += "\n" + maquinaCliente.getNome();
+		}
+
+		return mensagem;
+	}
+
+	/**
+	 * @author mayer
+	 * @return String
+	 */
+	public String renameUsuario(String novoNome){
+
+		if (!this.isNomeExistente(novoNome)) {
+			this.maquinaCliente.setNome(novoNome);
+			return "Usuário Renomeado com sucesso!";
+		}
+		
+		return "Nome de usuário já existente!";
+	}
+	/**
+	 * @author mayer
+	 * @param novoNome
+	 * @return boolean
+	 */
+	private boolean isNomeExistente(String novoNome){
+
+		for(MaquinaCliente maquinaCliente: this.maquinasClientes) {
+			
+			if (maquinaCliente.getNome().equals(novoNome)) {
+				return true;
+			}
+		}
+		
+		return false;
+	}
 }
--- TCP/ServidorTCP.java
+++ TCP/ServidorTCP.java
@@ -1,24 +1,27 @@
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.net.SocketException;
+import java.util.ArrayList;
+import java.util.List;
+
+import model.MaquinaCliente;
 
 public class ServidorTCP {
-
+	private static List<MaquinaCliente> maquinasClientesOnline = new ArrayList<MaquinaCliente>();
 	public static void main(String[] args) {
 		
-		//Cria o DatagramSocket para aguardar mensagens, neste momento o método fica bloqueando
-        //até o recebimente de uma mensagem
+		//Cria o DatagramSocket para aguardar mensagens, neste momento o mï¿½todo fica bloqueando
+        //atï¿½ o recebimente de uma mensagem
 		
 		try {
 			ServerSocket ss = new ServerSocket (6500);			
 			do{
 				Socket socket = ss.accept();
-				
-				ServerListener serverlis = new ServerListener(socket);
-				serverlis.start();
+				MaquinaCliente maquinaCliente = new MaquinaCliente(socket);
+				maquinasClientesOnline.add(maquinaCliente);
+				ServerListener serverListener = new ServerListener(maquinaCliente,maquinasClientesOnline);
+				serverListener.start();
 				
 			}while(true);
 			
